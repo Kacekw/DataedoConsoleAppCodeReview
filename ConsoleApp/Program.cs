8a44178784506abc92c43df123c0a145335a19f7
Kacper Witos <kawit@vestas.com>
2023-03-01 20:33:57 +0100

    Main method modified to take two arguments; - relative filepath to the file - printing flag (stringified boolean)
    Application will now rely on the users input for the file name, instead of hard-coding it inside the main method.
    Stringified flag will be converted to bool primitive. If convertion fails, specified message should be displayed before throwing exception message.
    
    This course of action will make the application more usable when it comes to handling files with different name than data.csv - and avoiding hard-coding spelling errors, as that was the case here.
    
    Project properties configured to pass proper arguments on Debug, thereby *.csproj.user file was commented out from .gitignore file.
    
    Unused "using" statements were removed. Ignored by the msbuild, though it makes the file a little bit more readable.
    
    data.csv file built action changed to "copy if newer". As it makes no bigger difference with small projects, it would when the solution grows - it may save some time re-building the solution.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,18 +1,43 @@
 ﻿namespace ConsoleApp
 {
     using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            string filePath = GetFilePath(args);
+
+            bool printingFlagConverted = args.Length > 1 ? ConvertPrintingFlag(args[1]) : true;
+
             var reader = new DataReader();
-            reader.ImportAndPrintData("dataa.csv");
+            reader.ImportAndPrintData(filePath, printingFlagConverted);
+        }
+
+        private static string GetFilePath(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new ArgumentException("File not specified");
+            }
+            else
+            {
+                return args[0];
+            }
+        }
+
+        private static bool ConvertPrintingFlag(string printingFlag)
+        {
+            try
+            {
+                return Convert.ToBoolean(printingFlag);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine($"Setting for printing was not recognized as a valid boolean value: {printingFlag}");
+
+                throw fe;
+            }
         }
     }
 }
